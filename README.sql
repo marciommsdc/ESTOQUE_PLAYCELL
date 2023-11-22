4da273adf410cc6a9242f1627a317674a37f88b8
marciommsdc <117790520+marciommsdc@users.noreply.github.com>
2023-11-22 10:05:39 -0300

    Update and rename README.md to README.sql

--- README.sql
+++ README.sql
@@ -6,3 +6,8 @@ categoria, sub-categoria e preço, a fim de se obter material para querys e trei
 
 Esse é o meu primeiro projeto em SQL.*/
 
+/*The database will consist of listing all items in the store separated by name, brand, model, type,
+category, sub-category and price, in order to obtain material for queries and personal training.
+
+This is my first project in SQL.*/
+

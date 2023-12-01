2408a5af7e6ae44cf47b07290c6fc3c2aac86d1e
marciommsdc <117790520+marciommsdc@users.noreply.github.com>
2023-11-30 21:55:44 -0300

    Update querys.sql

--- querys.sql
+++ querys.sql
@@ -1,6 +1,23 @@
---Listando todos os produtos do Fabricante Amazon.
---Listing all products from Amazon brand.
+/*Listando todos os produtos do Fabricante Amazon.
+Listing all products from Amazon brand.*/
 
 SELECT [COD_PRODUTO] AS [Código do produto], [FABRICANTE] AS [Fabricante]
 FROM [PRODUTOS]
 WHERE [FABRICANTE] = 'Amazon'
+
+/*Está chegando o aniversário da Tech Store e o chefe resolveu dar 10% de desconto em todas 
+as peças da cateria de hardware. Para isso ele chamou o Marcio que mexe no banco de dados
+da empresa e pediu para ele alterar todos estes produtos com um só comando no SQL.
+
+Como Marcio resolveu essa questão?*/
+
+/*The Tech Store's anniversary is coming up, and the boss decided to offer a 10% discount on all 
+hardware category items. For this, he called Marcio, who works with the company's database, and 
+asked him to change all these products with a single SQL command
+
+How did Marcio solve this issue?*/
+
+UPDATE [PRODUTOS]
+SET [PRECO] = [PRECO] * 0.9
+WHERE [CATEGORIA] = 'Hardware'
+
